65fb98c7c5c82418ab2ad79f79254b76872b57d3
user <user@email.ru>
2022-07-16 10:49:31 -0700

    from flask import Flask ModuleNotFoundError: No module named 'flask C:\Users\mikbe\Pycharm\skypro_lesson_10_2\venv>pip freezecertifi==2022.6.15 charset-normalizer==2.0.12 click==8.1.3 colorama==0.4.5 Flask==2.1.3 idna==3.3 itsdangerous==2.1.2 Jinja2==3.1.2 MarkupSafe==2.1.1 requests==2.28.0 urllib3==1.26.9 Werkzeug==2.1.2

--- my_flask_min.py
+++ my_flask_min.py
@@ -9,10 +9,21 @@ def hello():
 app.run('0.0.0.0', 8000)
 
 
-  from flask import Flask
-ModuleNotFoundError: No module named 'flask'
-
-
-
+#   from flask import Flask
+# ModuleNotFoundError: No module named 'flask'
+#
+# C:\Users\mikbe\Pycharm\skypro_lesson_10_2\venv>pip freeze
+# certifi==2022.6.15
+# charset-normalizer==2.0.12
+# click==8.1.3
+# colorama==0.4.5
+# Flask==2.1.3
+# idna==3.3
+# itsdangerous==2.1.2
+# Jinja2==3.1.2
+# MarkupSafe==2.1.1
+# requests==2.28.0
+# urllib3==1.26.9
+# Werkzeug==2.1.2
 
 
